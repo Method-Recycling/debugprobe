41b323adf648b93dd3bbe41b2bcbbad068a976bc
ZacharyRogers <zachary@methodrecycling.com>
2024-08-19 09:38:39 +1200

    tidy

--- src/loadcell.c
+++ src/loadcell.c
@@ -1,15 +1,23 @@
 #include "loadcell.h"
 
-uint16_t modbus_crc16(const uint8_t *data, uint16_t length) {
+
+/* Checksum stuff */
+static uint16_t modbus_crc16(const uint8_t *data, uint16_t length)
+{
     uint16_t crc = 0xFFFF;
 
-    for (uint16_t i = 0; i < length; i++) {
+    for (uint16_t i = 0; i < length; i++)
+	{
         crc ^= data[i];
 
-        for (uint8_t j = 0; j < 8; j++) {
-            if (crc & 0x0001) {
+        for (uint8_t j = 0; j < 8; j++)
+		{
+            if (crc & 0x0001)
+			{
                 crc = (crc >> 1) ^ 0xA001;
-            } else {
+            }
+			else
+			{
                 crc >>= 1;
             }
         }
@@ -18,9 +26,33 @@ uint16_t modbus_crc16(const uint8_t *data, uint16_t length) {
     return crc;
 }
 
+
+static void append_checksum(uint8_t* buf, uint16_t buf_len)
+{
+	uint16_t crc = modbus_crc16(buf, buf_len - 2);
+	buf[buf_len - 2] = crc & 0xFF;
+	buf[buf_len - 1] = (crc >> 8) & 0xFF;
+}
+
+
+/* Check that the calculated checksum matches the received one */
+static bool verify_checksum(const uint8_t* buf, const uint8_t buf_len)
+{
+	uint16_t crc_calc = modbus_crc16(buf, buf_len - sizeof(uint16_t));
+	uint16_t crc_rxd  = (buf[buf_len - 2] | (buf[buf_len - 1] << 8));
+
+	if(crc_calc != crc_rxd)
+	{
+		return false;
+	}
+
+	return true;
+}
+
+
+/* Loadcell stuff */
 typedef enum {
-    WEIGHT_DATA_ADDR = (uint16_t)0x0001,
-    // ZERO_DATA_ADDR   = (uint16_t)0x0001,
+    WEIGHT_DATA_ADDR = (uint16_t)0x0001, //ZERO_DATA_ADDR shares the same address
     ACCEL_ADDR       = (uint16_t)0x0009,
     ID_RATE_ADDR     = (uint16_t)0x000B,
     FILTER_ADDR      = (uint16_t)0x000D,
@@ -32,7 +64,6 @@ typedef enum {
     READ_DATA_CODE   = (uint8_t)0x03,
     WRITE_DATA_CODE  = (uint8_t)0x10
 } gpb100d_fn_code_e;
-
 
 
 struct loadcell_ctx
@@ -47,30 +78,124 @@ struct loadcell_ctx
 	uint8_t address_buf[9];
 };
 
-static struct loadcell_ctx ctx = {};
+
+static struct loadcell_ctx ctx = {
+	.address = 1,
+	.current_weight = 0
+};
+
 
 TaskHandle_t loadcell_taskhandle;
-
-
-static uint8_t tx_buf[64];
 static uint8_t rx_buf[64];
 
-void send_weight_response()
+/* Response handlers */
+static void send_weight_response()
 {
-	uint8_t buf[] = {0x01, 0x03, 0x04, 0x01, 0x31, 0xD4, 0xC0, 0xF5, 0x50};
-	uart_write_blocking(LOADCELL_UART_INTERFACE, buf, 9);
+	uint8_t buf[] = {ctx.address, 0x03, 0x04, 0x01, 0x31, 0xD4, 0xC0, 0x00, 0x00};
+	append_checksum(buf, sizeof(buf));
+	uart_write_blocking(LOADCELL_UART_INTERFACE, buf, sizeof(buf));
 }
 
-void send_zero_response();
-void send_accel_response();
-void send_id_rate_response();
-void send_filter_param_response();
-void send_address_response();
+
+static void send_zero_response()
+{
+	uint8_t buf[] = {ctx.address, 0x10, 0x00, 0x01, 0x00, 0x02, 0x00, 0x00};
+	append_checksum(buf, sizeof(buf));
+	uart_write_blocking(LOADCELL_UART_INTERFACE, buf, sizeof(buf));
+}
+
+
+static void send_accel_response()
+{
+	uint8_t buf[] = {ctx.address, 0x10, 0x00, 0x09, 0x00, 0x02, 0x00, 0x00};
+	append_checksum(buf, sizeof(buf));
+	uart_write_blocking(LOADCELL_UART_INTERFACE, buf, sizeof(buf));
+}
+
+
+static void send_id_rate_response()
+{
+
+}
+
+
+static void send_filter_param_response()
+{
+
+}
+
+
+static void send_address_response()
+{
+	uint8_t buf[] = {ctx.address, 0x10, 0x00, 0x0F, 0x00, 0x02, 0x00, 0x00};
+	append_checksum(buf, sizeof(buf));
+	uart_write_blocking(LOADCELL_UART_INTERFACE, buf, sizeof(buf));
+}
 
 
 static void receive_enable(bool enable)
 {
     gpio_put(LOADCELL_UART_RE, !enable);
+}
+
+
+static void read_data(uint8_t id, gpb100d_address_e addr)
+{
+	switch (addr)
+	{
+	case WEIGHT_DATA_ADDR: send_weight_response();
+	case ACCEL_ADDR:
+	case ID_RATE_ADDR:
+	case FILTER_ADDR:
+	case ADDR_SET_ADDR:
+	default:
+		break;
+	}
+}
+
+
+static void write_data(uint8_t id, gpb100d_address_e addr)
+{
+	switch (addr)
+	{
+	case WEIGHT_DATA_ADDR:
+		break;
+	case ACCEL_ADDR:
+	case ID_RATE_ADDR:
+	case FILTER_ADDR:
+	case ADDR_SET_ADDR:
+	default:
+		break;
+	}
+}
+
+
+static void process_message(uint8_t* buf, uint8_t buf_len)
+{
+	receive_enable(false);
+
+	if(buf_len < 4)
+	{
+		return;
+	}
+
+	uint8_t  request_id = buf[0];
+	uint16_t fn_code    = buf[1];
+	uint16_t reg_addr 	= ((buf[2] << 8) & 0xFF) | (buf[3] & 0xFF);
+
+	if(request_id != ctx.address || !verify_checksum(buf, buf_len))
+	{
+		return;
+	}
+
+	switch ((gpb100d_address_e)fn_code)
+	{
+		case READ_DATA_CODE:  read_data(request_id, reg_addr);
+		case WRITE_DATA_CODE: write_data(request_id, reg_addr);
+		default:              break;
+	}
+
+	receive_enable(true);
 }
 
 
@@ -90,79 +215,6 @@ void loadcell_init(void)
 }
 
 
-void read_data(uint8_t id, gpb100d_address_e addr)
-{
-	switch (addr)
-	{
-	case WEIGHT_DATA_ADDR: send_weight_response();
-	case ACCEL_ADDR:
-	case ID_RATE_ADDR:
-	case FILTER_ADDR:
-	case ADDR_SET_ADDR:
-	default:
-		break;
-	}
-}
-
-
-void write_data(uint8_t id, gpb100d_address_e addr)
-{
-	switch (addr)
-	{
-	case WEIGHT_DATA_ADDR: break;
-	case ACCEL_ADDR:
-	case ID_RATE_ADDR:
-	case FILTER_ADDR:
-	case ADDR_SET_ADDR:
-	default:
-		break;
-	}
-}
-
-
-/* Check that the calculated checksum matches the received one */
-static bool verify_checksum(const uint8_t* buf, const uint8_t buf_len)
-{
-	uint16_t crc_calc = modbus_crc16(buf, buf_len - sizeof(uint16_t));
-	uint16_t crc_rxd  = (buf[buf_len - 2] | (buf[buf_len - 1] << 8));
-
-	if(crc_calc != crc_rxd)
-	{
-		// LOG_ERR("CRC didn't match: Calc: 0x%04x, RX'd: 0x%04x", crc_calc, crc_rxd);
-		return false;
-	}
-
-	return true;
-}
-
-
-void process_message(uint8_t* buf, uint8_t buf_len)
-{
-	// receive_enable(false);
-	if(buf_len < 4)
-	{
-		return;
-	}
-
-	uint8_t           request_id = buf[0];
-	gpb100d_fn_code_e fn_code    = buf[1];
-	uint16_t reg_addr 	 = ((buf[2] << 8) & 0xFF) | (buf[3] & 0xFF);
-
-	if(!verify_checksum(buf, buf_len))
-	{
-		return;
-	}
-
-	switch ((gpb100d_address_e)fn_code)
-	{
-	case READ_DATA_CODE:  read_data(request_id, reg_addr);
-	case WRITE_DATA_CODE: write_data(request_id, reg_addr);
-	default:              break;
-	}
-	// receive_enable(true);
-}
-
-
 bool loadcell_task(void)
 {
     uint rx_len = 0;
@@ -175,10 +227,7 @@ bool loadcell_task(void)
 	if(rx_len)
 	{
 		process_message(rx_buf, rx_len);
-		// uint8_t buf[] = {0x01, 0x03, 0x04, 0x01, 0x31, 0xD4, 0xC0, 0xF5, 0x50};
-		// uart_write_blocking(LOADCELL_UART_INTERFACE, rx_buf, rx_len);
 	}
-
 
     return false;
 }
